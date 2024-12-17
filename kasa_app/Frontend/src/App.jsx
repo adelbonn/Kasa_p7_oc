c2e5b774f6eb16135d46727bb3c4fea56be89db1
adeline bonnamour <ad.bonna@outlook.fr>
2024-12-17 23:25:30 +0100

    structure projet

--- kasa_app/Frontend/src/App.jsx
+++ kasa_app/Frontend/src/App.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Home from './'
+import Home from './pages/Home';
+import About from './pages/About';
+import notFound from './pages/notFound';
+import LogementDetails from './pages/LogementDetails';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import '../src/styles/App.css';//importer les autres styles ensuite
+
 
 
