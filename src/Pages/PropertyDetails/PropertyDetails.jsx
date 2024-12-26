c007d2b8dba4cdece64e7a39324c8fca3bf252b5
adeline bonnamour <ad.bonna@outlook.fr>
2024-12-26 04:19:12 +0100

    ajout de la route pour Propertydetail

---
+++ src/Pages/PropertyDetails/PropertyDetails.jsx
@@ -0,0 +1,13 @@
+import Layout from '../../Components/Layout/Layout';
+// import './PropertyDetails.module.css'; pas necessaire de mettre un css etant donné que j'ai dejà le ficheir index.css pour le style global et aussi le App et le layout, en fait faut que je fasse un petit tri
+
+const PropertyDetails = () => {
+    return (
+        <Layout>
+            <div>
+                <h1>PropertyDetails</h1>
+            </div>
+        </Layout>
+    )
+}
+export default PropertyDetails;
