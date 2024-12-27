705295dcc44071199bc735e8dc8449598d01fb84
adeline bonnamour <ad.bonna@outlook.fr>
2024-12-27 18:10:22 +0100

    reafctor code composant gallery, page Home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,19 +1,30 @@
 
-import GalleryContainer from '../../Components/GalleryContainer/GalleryContainer';
 import Layout from '../../Components/Layout/Layout';
-// import HeroHeader from '../../Components/HeroHeader/HeroHeader';
-// import heroImage from '../../assets/img/HeroImgHome.png';
-// import Header from '../../Components/Header/Header';
 import Gallery from '../../Components/Gallery/Gallery';
-
+import {useEffect, useState } from 'react';
+import data from '../../Data/DataLogements.json';
+import Card from '../../Components/Card/Card';
 const Home = () => {
+
+    const [logements, setLogements] = useState([]);  //ici j'initialise mon state logements avec un tableau vide, j'utilise useState pour chargé ls données de mon fichier json, et donc setLogement() va me permettre de mettre a jour mon state logements avec les données de mon fichier json
+ 
+    useEffect(() =>{
+        setLogements(data);
+    }, []);
+    // j'utilise useEffect pour effectué les effets de bords, je l utilise pour chargé les données quand le composant card est monté, et donc je passe un tableau vide en second paramètre pour que cela ne s'execute qu'une seule fois après le premier rendu du composant card
+    
     return (
        
      <Layout page='home'>
-      {/* <GalleryContainer> */}
-      <Gallery />
-      {/* </GalleryContainer> */}
-        </Layout>
+        <Gallery>
+           {logements.map((logement) => 
+        <Card 
+         key={logement.id}
+         logement={logement} 
+         />
+         )}
+        </Gallery>
+    </Layout>
     );
 };
 
--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,15 +1,18 @@
 
 import {NavLink } from 'react-router-dom';
-// import styles from './NotFound.module.css';
+import Layout from '../../Components/Layout/Layout';
+ import styles from './NotFound.module.css';
 //mettre Link pour tester les differnces
 
 const NotFound = () => {
     return (
-        <div className={styles.container}>
-        <h1>404</h1>
+        <Layout page='notFound'>
+        <div>
+        <h1 className={styles.NotFoundH1}>404</h1>
         <h2>Oups! La page que vous demandez n&apos;existe pas.</h2>
         <NavLink to="/">Retourner sur la page d&apos;accueil</NavLink>
         </div>
+        </Layout>
     );
 };
 export default NotFound;
